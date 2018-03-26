f604e7010bbdc10dc43041f94a10cd416f7540a6
kamalenduDas <32879190+kamalenduDas@users.noreply.github.com>
2018-03-26 09:44:49 +0530

    Delete database.php

--- database/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-// connection to mysql
-$con = mysqli_connect("localhost","root","MRROBOT","bookmysport");
-if(mysqli_connect_errno()){
-
-  die("failed to connect".mysqli_connect_error());
-}
-?>
